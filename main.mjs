772212826e7d8e8279da47a43e28e9a7801f632b
drumath <2237k.drumath@gmail.com>
2024-11-11 01:47:00 +0900

    :sparkles: ちょっと修正

--- main.mjs
+++ main.mjs
@@ -3,11 +3,8 @@ import fs from "node:fs/promises";
 const main = async () => {
   const wasmCode = await fs.readFile("./build/hello.wasm");
 
-  const { instance, module } = await WebAssembly.instantiate(wasmCode, {});
+  const { instance } = await WebAssembly.instantiate(wasmCode, {});
   const helloFunc = instance.exports.hello;
-
-  console.log(instance.exports);
-
   const result = helloFunc();
   console.log(result);
 };
