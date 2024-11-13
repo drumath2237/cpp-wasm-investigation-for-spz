f33ee935de77a2747e339b4d61adb8929e2ada52
drumath2237 <2237k.drumath@gmail.com>
2024-11-13 20:16:34 +0900

    :tada: 実はエントリポイントを定義してビルドされたjsを実行すればspzが動くことが分かった
    しかし、wasmを別のjsから読もうとするとなぜか失敗する。なにがちがうんだろう

--- emcc-sandbox/main.cpp
+++ emcc-sandbox/main.cpp
@@ -1,9 +1,10 @@
 #include <emscripten.h>
 
-// #include "spz/src/cc/splat-types.h"
+#include "spz/src/cc/splat-types.h"
 #include "spz/src/cc/splat-types.cc"
-// #include "spz/src/cc/load-spz.h"
-// #include "spz/src/cc/load-spz.cc"
+
+#include "spz/src/cc/load-spz.h"
+#include "spz/src/cc/load-spz.cc"
 
 #include <zlib.h>
 
@@ -12,34 +13,49 @@ using namespace std;
 
 extern "C"
 {
-  // EMSCRIPTEN_KEEPALIVE
-  // int fib(int n)
-  // {
-  //   if (n <= 0)
-  //   {
-  //     return 0;
-  //   }
-  //   int i, t, a = 0, b = 1;
-  //   for (i = 1; i < n; i++)
-  //   {
-  //     t = a + b;
-  //     a = b;
-  //     b = t;
-  //   }
-  //   return b;
-  // }
-
   EMSCRIPTEN_KEEPALIVE
   float hoge()
   {
-    // spz::GaussianCloud gs = {
-    //     43, 0, false, {0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}, {1.0, 0.0, 0.0, 0.0}, {1.0}, {1.0, 1.0, 1.0}, {0.0}};
+    spz::GaussianCloud gs = {
+        43, 0, false, {0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}, {1.0, 0.0, 0.0, 0.0}, {1.0}, {1.0, 1.0, 1.0}, {0.0}};
 
-    spz::Vec3f v = {1, 2, 3};
-    spz::Vec3f normalized = spz::normalized(v);
-    return normalized[1];
+    return gs.numPoints;
+
+    spz::Vec3f v1 = {1, 2, 3};
+    spz::Vec3f v2 = {2, 3, 4};
+
+    spz::Vec3f res = spz::plus(v1, v2);
+    return spz::norm(res);
     // return 1;
     //   return gs.data().shDegree;
     // return gs.numPoints;
   }
+}
+
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+  spz::GaussianCloud gs = {
+    1, 0, false,
+    {0.0, 0.0, 0.0},
+    {1.0, 1.0, 1.0},
+    {1.0, 0.0, 0.0, 0.0},
+    {1.0},
+    {1.0, 1.0, 1.0},
+    {}
+  };
+
+  vector<uint8_t> bin;
+  spz::saveSpz(gs, &bin);
+
+  for(int i=0; i<bin.size(); i++)
+  {
+    printf("%d, ", bin[i]);
+  }
+  printf("\n");
+  
+
+  return 0;
 }
