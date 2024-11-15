54fa4df14baacc095389b19bd525ee3c80b787ee
drumath2237 <2237k.drumath@gmail.com>
2024-11-15 19:34:00 +0900

    :sparkles: ems側でmallocやfreeを出力させた

--- emcc-sandbox/build.sh
+++ emcc-sandbox/build.sh
@@ -12,4 +12,5 @@ em++ main.cpp \
  -s WASM=1 \
  -s MODULARIZE=1 \
  -s EXPORT_ES6=1 \
- -s ALLOW_MEMORY_GROWTH
+ -s ALLOW_MEMORY_GROWTH \
+ -s EXPORTED_FUNCTIONS="['_malloc', '_free']"
--- emcc-sandbox/index.mjs
+++ emcc-sandbox/index.mjs
@@ -2,20 +2,26 @@ import { readFile } from "fs/promises";
 import Module from "./build/main.mjs";
 
 const main = async () => {
-  const { save_spz, load_spz } = await Module();
+const { save_spz, load_spz, HEAP8, _malloc, _free } = await Module();
 
-  // const buf = save_spz(0);
-  // console.log(buf.size());
+// const buf = save_spz(0);
+// console.log(buf.size());
 
-  // for (let i = 0; i < buf.size();i++)
-  // {
-  //   console.log(buf.get(i));
+// for (let i = 0; i < buf.size();i++)
+// {
+//   console.log(buf.get(i));
 
-  // }
+// }
 
-  const gsData = await readFile("./spz/samples/racoonfamily.spz");
-  const gaussianCloud = load_spz(new Uint8Array(gsData.buffer));
-  console.log(gaussianCloud);
+// const gsData = await readFile("./spz/samples/racoonfamily.spz");
+// const gaussianCloud = load_spz(new Uint8Array(gsData.buffer));
+// console.log(gaussianCloud);
+
+console.log(HEAP8)
+console.log(_malloc);
+console.log(_free);
+
+
 };
 
 main();
